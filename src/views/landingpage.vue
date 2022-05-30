c3936c102af8e3f9ec807a512fbe51d680c0baf0
ardhanshahab <ardhanshahab@gmail.com>
2022-05-30 21:03:28 +0700

    feat : testing design landingpage

--- src/views/landingpage.vue
+++ src/views/landingpage.vue
@@ -1,5 +1,26 @@
 <template>
-    <div></div>
+    <div class="bodyimage">
+        <b-container>
+        <div class="row">
+            <b-row align-v="center">
+            <b-col cols="1"></b-col>
+            <b-col cols="10" align-self="center">
+        <b-card img-src="https://placekitten.com/300/300" img-alt="Card image" img-left class="center text-center">
+            <b-card-title>Holy Hospital</b-card-title>
+            <div class="my-4">
+            <b-button block variant="primary">Login</b-button>
+            </div>
+            <div class="my-4">
+            <b-button block variant="primary">Get Username</b-button>
+            </div>
+    </b-card>
+    </b-col>
+    <b-col cols="1"></b-col>
+    </b-row>
+  </div>
+  <div></div>
+  </b-container>
+    </div>
 </template>
 
 <script>
@@ -7,3 +28,14 @@ export default {
     name: "landingPage"
 }
 </script>
+
+<style scoped>
+.bodyimage {
+    background-image: url("/src/assets/bglp.jpg");
+    height: auto;
+}
+.center {
+  margin-top: 50%;
+  margin-bottom:50%;
+}
+</style>
