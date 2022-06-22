20c390013789f0314323d1ecd0da2fcb2ece769f
ardhanshahab <ardhanshahab@gmail.com>
2022-06-22 20:28:03 +0700

    feat : finishing outpatient (revisi addoutpatient 2)

--- src/views/outpatientadd.vue
+++ src/views/outpatientadd.vue
@@ -8,30 +8,115 @@
         <b-card bg-variant="light" class="card text-center mx-2 my-2 text-purple">
           <b-row class="my-3">
             <b-col cols="2">
-              <label class="mt-2" aria-controls="fieldset-1">kodepasien</label>
+              <label class="mt-2" aria-controls="fieldset-1">Kode Pasien</label>
             </b-col>
               <b-col cols="10">
               <b-form-group
                 id="fieldset-1"
-                :invalid-feedback="invalidFeedback"
+                :invalid-feedback="invalidfeedbackKP"
                 :state="state"
               >
-          <b-form-input id="input-1" v-model="name" :state="state" trim></b-form-input>
+          <b-form-input id="input-1" v-model="kode_pasien" :state="stateKP" trim></b-form-input>
         </b-form-group>
               </b-col>
           </b-row>
 
           <b-row class="my-3">
             <b-col cols="2">
-              <label class="mt-2" aria-controls="fieldset-1">kodepasien</label>
+              <label class="mt-2" aria-controls="fieldset-2">Nama Pasien</label>
             </b-col>
               <b-col cols="10">
               <b-form-group
                 id="fieldset-2"
-                :invalid-feedback="invalidFeedback"
-                :state="state"
+                :invalid-feedback="invalidfeedbackNP"
+                :state="stateNP"
               >
-          <b-form-input id="input-2" v-model="name" :state="state" trim></b-form-input>
+          <b-form-input id="input-2" v-model="nama_pasien" :state="stateNP" trim></b-form-input>
+        </b-form-group>
+              </b-col>
+          </b-row>
+
+        <b-row class="my-3">
+            <b-col cols="2">
+              <label class="mt-2" aria-controls="fieldset-5">Tanggal Kontrol</label>
+            </b-col>
+              <b-col cols="10">
+           <b-form-datepicker id="example-datepicker" v-model="tanggal_kontrol" class="mb-2"></b-form-datepicker>
+              </b-col>
+          </b-row>
+          <b-row class="my-3">
+            <b-col cols="2">
+              <label class="mt-2" aria-controls="fieldset-3">Keluhan</label>
+            </b-col>
+              <b-col cols="10">
+              <b-form-group
+                id="fieldset-3"
+                :invalid-feedback="invalidfeedbackKel"
+                :state="stateKel"
+              >
+          <b-form-input id="input-3" v-model="keluhan" :state="stateKel" trim></b-form-input>
+        </b-form-group>
+              </b-col>
+          </b-row>
+
+          <b-row class="my-3">
+            <b-col cols="2">
+              <label class="mt-2" aria-controls="fieldset-6">Jenis Poli</label>
+            </b-col>
+              <b-col cols="10">
+                <b-form-select
+                          id="input-6"
+                          v-model="jenis_poli"
+                          :options="poli"
+                          required
+                          plain
+                        ></b-form-select>
+              </b-col>
+          </b-row>
+
+          <b-row class="my-3">
+            <b-col cols="2">
+              <label class="mt-2" aria-controls="fieldset-6">Jadwal Sesi</label>
+            </b-col>
+              <b-col cols="10">
+                <b-form-select
+                          id="input-6"
+                          v-model="jadwal_sesi"
+                          :options="sesi"
+                          required
+                          plain
+                        ></b-form-select>
+              </b-col>
+          </b-row>
+
+          <b-row class="my-3">
+            <b-col cols="2">
+              <label class="mt-2" aria-controls="fieldset-6">Nama Dokter</label>
+            </b-col>
+              <b-col cols="10">
+                <b-form-select
+                          id="input-6"
+                          v-model="nama_dokter"
+                          :options="dokter"
+                          required
+                          plain
+                          
+                        >
+                        </b-form-select>
+              </b-col>
+          </b-row>
+
+          <b-row class="my-3">
+            <b-col cols="2">
+              <label class="mt-2" aria-controls="fieldset-4">Nomor Antrian</label>
+            </b-col>
+              <b-col cols="10">
+              <b-form-group
+                id="fieldset-4"
+                :invalid-feedback="invalidfeedbackNA"
+                :state="stateNA"
+              >
+          <b-form-input id="input-4" v-model="nomor_antrian" :state="stateNA" trim></b-form-input>
         </b-form-group>
               </b-col>
           </b-row>
@@ -48,25 +133,59 @@
 export default {
     name: "outpatientAdd",
     computed: {
-      state() {
-        return this.name.length >= 4
+      stateKP() {
+        return this.kode_pasien.length >= 4
       },
-      invalidFeedback() {
-        if (this.name.length > 0) {
+      invalidfeedbackKP() {
+        if (this.kode_pasien.length > 0) {
           return 'Minimal 4 huruf characters.'
         }
-        return 'Harap masukan nama.'
+        return 'Harap masukan Kode Pasien.'
+      },
+      stateNP() {
+        return this.nama_pasien.length >= 4
+      },
+      invalidfeedbackNP() {
+        if (this.nama_pasien.length > 0) {
+          return 'Minimal 4 huruf characters.'
+        }
+        return 'Harap masukan Nama Pasien.'
+      },
+      stateKel() {
+        return this.keluhan.length >= 4
+      },
+      invalidfeedbackKel() {
+        if (this.keluhan.length > 0) {
+          return 'Minimal 4 huruf characters.'
+        }
+        return 'Tuliskan Keluhan anda.'
+      },
+      stateNA() {
+        return this.nomor_antrian.length >= 1
+      },
+      invalidfeedbackNA() {
+        if (this.nomor_antrian.length > 0) {
+          return 'Minimal 1 nomor.'
+        }
+        return 'Nomor antrain.'
       }
     },
     data() {
       return {
         name: '',
-        food: null,
-      poli: [{ text: 'Pilih Poli', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
-      sesi: [{ text: 'Pilih Sesi', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
-      dokter: [{ text: 'Pilih Dokter', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
+        kode_pasien: '',
+        nama_pasien: '',
+        keluhan: '',
+        nomor_antrian: '',
+        tanggal_kontrol: '',
+        jenis_poli: null,
+        jadwal_sesi: null,
+        nama_dokter: null,
 
-
+      poli: [{ text: 'Pilih Poli', value: null }, 'Umum', 'Anak', 'Gigi'],
+      sesi: [{ text: 'Pilih Sesi', value: null }, 'Pagi (08.00 - 11.00)', 'Siang (13.00 - 15.00)', 'Sore (16.00 - 18.00)'],
+      dokter: [{ text: 'Pilih Dokter', value: null }, 'dr. Alshad Ahmad', 'dr. Adira Putri', 'dr. Seno', 'dr. Christie'],
+    
 }
     }
   
