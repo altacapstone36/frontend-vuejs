a4b79375e5f84b27d099729c857afd58623760e9
ardhanshahab <ardhanshahab@gmail.com>
2022-06-24 18:17:19 +0700

    feat : finishing schedule(adddoctorschedule)

--- src/views/adddoctorschedule.vue
+++ src/views/adddoctorschedule.vue
@@ -1,64 +1,84 @@
 <template>
 <div class="container my-2">
         <div class="d-flex text-center">
-            <img src="@/assets/Icon/add doctor schedule.svg" width="70"/>
+            <!-- <img src="@/assets/Icon/add doctor schedule.svg" width="70"/> -->
             <h3 class="textmargin">ADD DOCTOR SCHEDULE</h3>
         </div>
         <div class="d-block">
             <b-card bg-variant="light" class="card text-center mx-2 my-2 text-purple">
-            <b-form @submit="onSubmit" @reset="onReset">
-    <table class="table table-borderless">
-        <tbody>
-                        <tr class="tablerow">
-                <td><p class="textmargin">Jenis Poli</p></td>
-                <td>
-                <b-form-select
+            <!-- <b-form @submit="onSubmit" @reset="onReset"> -->
+<b-row class="my-3">
+            <b-col cols="2">
+              <label class="mt-2" aria-controls="fieldset-1">Jenis Poli</label>
+                </b-col>
+                <b-col cols="10">
+                        <b-form-select
                         id="input-3"
-                        v-model="food"
+                        v-model="jenis_poli"
                         :options="poli"
                         required
                         class="w-100"
                       ></b-form-select>
-                </td>
-            </tr>
-            <tr class="tablerow">
-                <td><p class="textmargin">Nama Dokter</p></td>
-                <td>
-                <b-form-select
+
+              </b-col>
+          </b-row>
+          <b-row class="my-3">
+            <b-col cols="2">
+              <label class="mt-2" aria-controls="fieldset-1">Nama Dokter</label>
+            </b-col>
+              <b-col cols="10">
+                        <b-form-select
                         id="input-3"
-                        v-model="food"
+                        v-model="nama_dokter"
                         :options="dokter"
                         required
                         class="w-100"
                       ></b-form-select>
-                </td>
-            </tr>
 
-             <tr class="tablerow">
-                <td><p class="textmargin">Pilih Hari</p></td>
-                <td>
-                <b-form-datepicker id="example-datepicker" v-model="value" class="mb-2"></b-form-datepicker>
-                </td>
-            </tr>
-            <tr class="tablerow">
-                <td><p class="textmargin">Pilih Sesi</p></td>
-                <td>
-                <b-form-select
+              </b-col>
+          </b-row>
+          <b-row class="my-3">
+            <b-col cols="2">
+              <label class="mt-2" aria-controls="fieldset-1">Jenis Perawatan</label>
+            </b-col>
+              <b-col cols="10">
+                            <b-form-group
+                                id="fieldset-1"
+                                :invalid-feedback="invalidfeedbackJP"
+                                :state="stateJP"
+                            >
+                        <b-form-input id="input-1" v-model="jenis_perawatan" :state="stateJP" trim></b-form-input>
+                        </b-form-group>
+
+              </b-col>
+          </b-row>
+          <b-row class="my-3">
+            <b-col cols="2">
+              <label class="mt-2" aria-controls="fieldset-1">Pilih Hari</label>
+            </b-col>
+              <b-col cols="10">
+               <b-form-datepicker id="example-datepicker" v-model="hari" class="mb-2"></b-form-datepicker>
+
+              </b-col>
+          </b-row>
+                <b-row class="my-3">
+                            <b-col cols="2">
+                            <label class="mt-2" aria-controls="fieldset-1">Pilih Sesi</label>
+                            </b-col>
+                            <b-col cols="10">
+                        <b-form-select
                         id="input-3"
-                        v-model="food"
+                        v-model="jadwal_sesi"
                         :options="sesi"
                         required
                         class="w-100"
                       ></b-form-select>
-                </td>
-            </tr>
-                    </tbody>
-
-    </table>
+                            </b-col>
+                        </b-row>
     <div class="d-flex mx-2 justify-content-end">
-    <b-button type="submit"  class="unguprimary btnset">Submit</b-button>
+    <b-button @click="onSubmit()"  class="unguprimary btnset">Submit</b-button>
     </div>
-    </b-form>
+    <!-- </b-form> -->
         </b-card>
         </div>
         
@@ -68,16 +88,35 @@
 <script>
 export default {
     name: 'adddoctorSchedule',
+    computed: {
+      stateJP() {
+        return this.jenis_perawatan.length >= 4
+      },
+      invalidfeedbackKP() {
+        if (this.jenis_perawatan.length > 0) {
+          return 'Minimal 4 huruf characters.'
+        }
+        return 'Harap masukan Kode Pasien.'
+      },
+
+    },
     data() {
       return {
-        name: '',
+        jenis_perawatan: '',
+        hari: '',
+        jadwal_sesi: '',
+        nama_dokter: '',
+        jenis_poli: '',
         food: null,
       poli: [{ text: 'Pilih Poli', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
       sesi: [{ text: 'Pilih Sesi', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
       dokter: [{ text: 'Pilih Dokter', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
-
-
-            }
+   }
+    },
+    methods: {
+        onSubmit() {
+            console.log(this.data.hari)
+        }
     }
 }
 </script>
